0c654505eb197a1b63d2bf1acc2ee2aad25c324d
arifarhan16 <45889833+arifarhan16@users.noreply.github.com>
2019-03-02 18:08:56 +0700

    Create toolbox.sh
    HOW TO USE 
    
    chmod +x toolbox.sh
    ./toolbox.sh

---
+++ toolbox.sh
@@ -0,0 +1,325 @@
+#!/bin/bash
+
+# GAUSAH RECODE KALO MAU DIHARGAI !
+# BIKIN TOOLS SUSAH JADI JANGAN BACOT AJG !
+# =========================================
+# Copyright @ Cy#b3r00q + Cyber00m 
+# HACKER KOK BUTA PROGRAM ! 
+
+
+
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+
+echo '';
+echo -e "\033[91m===============================";
+echo -e "\033[1;34mHOME OF INSTALLER TOOLS HACKING";
+echo -e "\033[91m===============================";
+echo -e "\033[1;36m     AUTO INSTALLER ";
+echo -e "\033[91m===============================";
+echo -e "\033[1;34m Author : Cy#b3r00q Ft Cy#b3r00M ";
+echo -e "\033[1;34m Team : IndoSec | BekasiSec ";
+echo -e "\033[1;34m Since : 2014 - 2019";
+echo -e "\033[91m===============================";
+sleep 1;
+echo '';
+echo -e "\033[1;34mReqruitment (WAJIB) : ";
+echo -e "\033[1;33m===[+]===";
+echo '';
+echo -e "\033[1;32m--GIT Install GIT ";
+echo -e "\033[1;32m--PHP Install PHP ";
+echo -e "\033[1;32m--PY Install PYTHON ";
+echo -e "\033[1;32m--PY2 Install PYTHON2 ";
+echo '';
+echo -e "\033[1;34mPilih Tools Yang Akan Diinstall : ";
+echo -e "\033[1;33m===[+]===";
+echo '';
+echo -e "\033[1;35m1. Install WebDav";
+echo -e "\033[1;35m2. Install CheckPort";
+echo -e "\033[1;35m3. Install SQLMAP";
+echo -e "\033[1;35m4. Install Admin Finder";
+echo -e "\033[1;35m5. Install Hammer ( DDoS ) ";
+echo -e "\033[1;35m6. Install Torshammer ( DDoS) ";
+echo -e "\033[1;35m7. Install Auto Fb Reaction";
+echo -e "\033[1;35m8. Install HASHKILLER ";
+
+echo -n -e "\033[1;34mRoot@User:~# ";
+read Pilihan
+
+if [ "$Pilihan" = '1' ]; 
+then
+git clone https://github.com/arifarhan16/webjav.git
+elif [ "$Pilihan" = '2' ]; then
+git clone https://github.com/arifarhan16/checker.git
+elif [ "$Pilihan" = '3' ]; then
+git clone https://github.com/sqlmapproject/sqlmap.git
+elif [ "$Pilihan" = '4' ]; then
+git clone https://github.com/arifarhan16/adfin.git
+elif [ "$Pilihan" = '5' ]; then
+git clone https://github.com/cyweb/hammer.git
+elif [ "$Pilihan" = '6' ]; then
+git clone https://github.com/dotfighter/torshammer.git
+elif [ "$Pilihan" = '7' ]; then
+git clone https://github.com/tomiashari/fb-autoreaction.git
+elif [ "$Pilihan" = '8' ]; then
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer.git
+elif [ "$Pilihan" = '--PHP' ]; then
+pkg install php
+elif [ "$Pilihan" = '--PY' ]; then
+pkg install python
+elif [ "$Pilihan" = '--PY2' ]; then
+pkg install python2
+elif [ "$Pilihan" = '--GIT' ]; then
+apt install git
+else 
+	echo " GAK ADA KOK MAKSAIN SIH ONII-CHAN >.<";
+fi
